c5d21a663391e1ef4827c5fff9c4970fd7a18925
Ruben Karlsson <neburgordon@gmail.com>
2021-05-14 16:16:38 +0200

    add services: auth functions in client

---
+++ client/src/services/auth.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const signup = async (username, password) => {
+  return axios
+    .post('/api/auth/signup', { username, password })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+const login = (username, password) => {
+  return axios
+    .post('/api/auth/login', { username, password })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+const logout = () => {
+  return axios
+    .delete('/api/auth/logout')
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err.response.data;
+    });
+};
+
+export { signup, logout, login };
